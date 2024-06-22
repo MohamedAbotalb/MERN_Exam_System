52ab0d7f772d7e6662a2e7c97f557c16dfdb3162
Nada Alnajdi <nada.abdo.alnajdi@gmail.com>
2024-06-22 21:14:18 +0300

    handle crud op for Questions

--- Frontend/src/components/AdminDashboard/QuestionTable.jsx
+++ Frontend/src/components/AdminDashboard/QuestionTable.jsx
@@ -1,17 +1,30 @@
-// QuestionTable.jsx
-
-import React from 'react';
-import { Table, Button } from 'react-bootstrap';
-import { useDispatch } from 'react-redux';
+import React, {useState } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
 import { deleteQuestion } from '../../store/questionsSlice';
+import { Table, Button } from 'react-bootstrap';
+import QuestionModal from './QuestionModal'; 
 
-const QuestionTable = ({ questions }) => {
+const QuestionTable = ({ questions,examId }) => {
+
   const dispatch = useDispatch();
+  const [showModal, setShowModal] = useState(false);
+  const [selectedQuestion, setSelectedQuestion] = useState(null);
+
   const handleDelete = (questionId) => {
     dispatch(deleteQuestion(questionId));
   };
 
+  const handleShowModal = (question) => {
+    setSelectedQuestion(question);
+    setShowModal(true);
+  };
+
+  const handleCloseModal = () => {
+    setSelectedQuestion(null);
+    setShowModal(false);
+  };
   return (
+    <div>
     <Table striped bordered hover>
       <thead>
         <tr>
@@ -29,17 +42,22 @@ const QuestionTable = ({ questions }) => {
             <td>{question.correctAnswer}</td>
 
             <td>
-              <Button variant="warning" onClick={() => console.log('Edit', question.id)}>
-                Edit
-              </Button>{' '}
-              <Button variant="danger" onClick={() => handleDelete(question.id)}>
-                Delete
-              </Button>
+            <td>
+                <Button variant="warning" onClick={() => handleShowModal(question)}>Edit</Button>{' '}
+                <Button variant="danger" onClick={() => handleDelete(question._id)}>Delete</Button>
+              </td>
             </td>
           </tr>
         ))}
       </tbody>
     </Table>
+    <QuestionModal
+        show={showModal}
+        onHide={handleCloseModal}
+        examId={examId}
+        questionToEdit={selectedQuestion}
+      />
+    </div>
   );
 };
 

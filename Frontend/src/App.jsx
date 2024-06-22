fd6cb8d0969de134b040a55ec4fc5c9dac510489
Mohamed Abotalb <mohamed.abotalb277@gmail.com>
2024-06-22 23:10:32 +0300

    Update App.jsx file

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,40 +1,43 @@
 import React from 'react';
-import { Provider } from 'react-redux';
 import { Routes, Route, Navigate } from 'react-router-dom';
 import { SharedLayout } from './layouts/SharedLayout';
 import AdminDashboard from './pages/AdminDashboard';
 import StudentDashboard from './pages/StudentDashboard';
-import store from './store/store';
-import "bootstrap/dist/css/bootstrap.min.css";
-import "bootstrap/dist/js/bootstrap.bundle";
 import Registration from './components/Registration';
 import Login from './components/Login';
+import Home from './pages/Home';
 // import Dashboard from './components/Dashboard';
 import ProtectedRoute from './components/ProtectedRoute';
 
-function App() {
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/js/bootstrap.bundle';
+import 'bootstrap-icons/font/bootstrap-icons.css';
 
+function App() {
   return (
     <Routes>
-      <Route path="/" element={<SharedLayout />}>
-        {/* <Route path="AdminDashboard/*" element={<AdminDashboard />} /> */}
-        {/* <Route path="StudentDashboard/*" element={<AdminDashboard />} /> */}
-         <Route exact path='/'  element={<ProtectedRoute  allowedRoles={['admin', 'user']}/>}>
-            <Route exact path='AdminDashboard/*' element={<AdminDashboard/>}/>
-        </Route> 
-         <Route exact path='/'  element={<ProtectedRoute  allowedRoles={['admin', 'user']}/>}>
-            <Route exact path='StudentDashboard/*' element={<AdminDashboard/>}/>
-        </Route> 
+      <Route path='/' element={<Home />} />
+      <Route path='/login' element={<Login />} />
+      <Route path='/register' element={<Registration />} />
+
+      <Route element={<SharedLayout />}>
+        <Route
+          path='AdminDashboard/*'
+          element={<ProtectedRoute allowedRoles={['admin']} />}
+        >
+          <Route index element={<AdminDashboard />} />
+        </Route>
+        <Route
+          path='StudentDashboard/*'
+          element={<ProtectedRoute allowedRoles={['user']} />}
+        >
+          <Route index element={<StudentDashboard />} />
+        </Route>
       </Route>
-        <Route path="/register" element={<Registration />} />
-        <Route path="/login" element={<Login/>} />
-        {/* <Route exact path='/'  element={<ProtectedRoute  allowedRoles={['admin', 'user']}/>}>
-            <Route exact path='/dashboard' element={<Dashboard/>}/>
-        </Route>                            */}
-        <Route path="/" element={<Navigate to="/login" />} />
-        <Route path="*" element={<Navigate to="/login" />} />
-        </Routes>
+
+      <Route path='*' element={<Navigate to='/login' />} />
+    </Routes>
   );
-};
+}
 
 export default App;

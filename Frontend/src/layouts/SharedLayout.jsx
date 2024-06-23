6feafce5b42b9fe0d7fb30bfb9a5ba37539bb997
Mohamed Abotalb <mohamed.abotalb277@gmail.com>
2024-06-23 06:54:30 +0300

    Update shard layouts

--- Frontend/src/layouts/SharedLayout.jsx
+++ Frontend/src/layouts/SharedLayout.jsx
@@ -2,24 +2,22 @@ import React from 'react';
 import { NavBar } from '../components/NavBar';
 import { Sidebar } from '../components/Sidebar';
 import { Outlet } from 'react-router-dom';
-import { Container, Row, Col } from 'react-bootstrap';
-import '../css/sidebar.css'
+import '../css/sidebar.css';
 
 export function SharedLayout() {
   return (
     <>
       <NavBar />
-
-    <div className="container-fluid">
-      <div className="row">
-        <div className="side p-0 col-2">
-          <Sidebar/>
-        </div>
-        <div className="col-10">
-          <Outlet/>
+      <div className='container-fluid'>
+        <div className='row'>
+          <div className='side p-0 col-2'>
+            <Sidebar />
+          </div>
+          <div className='col-10'>
+            <Outlet />
+          </div>
         </div>
       </div>
-    </div>
     </>
   );
 }

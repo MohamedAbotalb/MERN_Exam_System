381d554335f0648a22589ae483b072c10839e978
Mohamed Abotalb <mohamed.abotalb277@gmail.com>
2024-06-23 06:55:56 +0300

    Update Home page

--- Frontend/src/pages/Home.jsx
+++ Frontend/src/pages/Home.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import { NavLink } from 'react-router-dom';
 import { Container, Row, Col, Card, Button } from 'react-bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import './Home.css';
+import '../css/Home.css';
 
 function Home() {
   return (
